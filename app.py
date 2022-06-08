28b6eb7d8ae3fa14fb4a14d3877a09e170b4b0e0
GauravBurande <burandegaurav8899@gmail.com>
2022-06-08 19:42:44 +0600

    unfinished deploy

---
+++ app.py
@@ -0,0 +1,17 @@
+from asyncio import locks
+from flask import Flask, render_template, request
+import pandas as pd
+
+app = Flask(__name__)
+
+data = pd.read_csv('cleaned_house_data.csv')
+
+@app.route('/')
+def index():
+
+    locations = sorted(data['location'].unique())
+    return render_template('index.html',locations=locations)
+
+
+if __name__ == "__main__":
+    app.run(debug=True,port=5001)
